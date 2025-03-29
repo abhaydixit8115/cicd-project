e12a66f8b4972f11d471cae65a067d72e3e0fda4
abhaydixit8115@gmail.com <adixit7386@gmail.com>
2025-03-29 11:24:31 +0530

    added terraform

---
+++ terraform/Keypair.tf
@@ -0,0 +1,4 @@
+resource "aws_key_pair" "dove-key2" {
+  key_name   = "dove-key2"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBMrJsNxz7d/t4mQpdsQYIFrzWMbDSFuD8z+1hvcUgpy adixit7386@acer"
+}
